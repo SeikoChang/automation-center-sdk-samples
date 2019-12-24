df078b70eb05a60a0fabb0ed09e9ccb7c3d8e788
SeikoChang <seiko_chang@trend.com.tw>
2019-12-24 18:49:08 +0800

    move json related functions to utility

--- python/src/_utility.py
+++ python/src/_utility.py
@@ -198,7 +198,6 @@ def deserialize_from_object(data, klass):
         else:
             return deserialize_model_(data, klass)
 
-
 def deserialize_file_(response):
     """Deserializes body to file
 
@@ -348,3 +347,105 @@ def deserialize_model_(data, klass):
 
     return instance
 
+def json_update_value_by_key(json, k, v):
+    if isinstance(json, dict):
+        for key in json.keys():
+            if key == k:
+                json[k] = v
+            elif isinstance(json[key], dict):
+                json_update_key_name(json[key], k, v)
+            elif isinstance(json[key], list):
+                for item in json[key]:
+                    json_update_key_name(item, k, v)
+    elif isinstance(json, list):
+        for item in json:
+            json_update_key_name(item, k, v)
+
+    return json
+
+def json_update_value_by_new_key_if_needed(json, k, v1, v2):
+    if isinstance(json, dict):
+        for key in json.keys():
+            if key == k:
+                if json[k] == v1:
+                    json[k] = v2
+            elif isinstance(json[key], dict):
+                json_update_value_by_new_key_if_needed(json[key], k, v1, v2)
+            elif isinstance(json[key], list):
+                for item in json[key]:
+                    json_update_value_by_new_key_if_needed(item, k, v1, v2)
+    elif isinstance(json, list):
+        for item in json:
+            json_update_value_by_new_key_if_needed(item, k, v1, v2)
+
+    return json
+
+def json_update_key_name(json, k, new_key):
+    if isinstance(json, dict):
+        for key in json.keys():
+            if key == k:
+                json[new_key] = json.pop(key)
+            elif isinstance(json[key], dict):
+                json_update_key_name(json[key], k, new_key)
+            elif isinstance(json[key], list):
+                for item in json[key]:
+                    json_update_key_name(item, k, new_key)
+    elif isinstance(json, list):
+        for item in json:
+            json_update_key_name(item, k, new_key)
+
+    return json
+
+def extrat_element_from_json(obj, path):
+    '''
+    Extracts an element from a nested dictionary or
+    a list of nested dictionaries along a specified path.
+    If the input is a dictionary, a list is returned.
+    If the input is a list of dictionary, a list of lists is returned.
+    obj - list or dict - input dictionary or list of dictionaries
+    path - list - list of strings that form the path to the desired element
+    '''
+    def extract(obj, path, ind, arr):
+        '''
+            Extracts an element from a nested dictionary
+            along a specified path and returns a list.
+            obj - dict - input dictionary
+            path - list - list of strings that form the JSON path
+            ind - int - starting index
+            arr - list - output list
+        '''
+        key = path[ind]
+        if ind + 1 < len(path):
+            if isinstance(obj, dict):
+                if key in obj.keys():
+                    extract(obj.get(key), path, ind + 1, arr)
+                else:
+                    arr.append(None)
+            elif isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        extract(item, path, ind, arr)
+            else:
+                arr.append(None)
+        if ind + 1 == len(path):
+            if isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        arr.append(item.get(key, None))
+            elif isinstance(obj, dict):
+                arr.append(obj.get(key, None))
+            else:
+                arr.append(None)
+        return arr
+
+    if isinstance(obj, dict):
+        return extract(obj, path, 0, [])
+    elif isinstance(obj, list):
+        outer_arr = []
+        for item in obj:
+            outer_arr.append(extract(item, path, 0, []))
+        return outer_arr
