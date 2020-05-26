29f7eac377f4ba342c7c7e8e72806981458db59d
SeikoChang <seiko_chang@trend.com.tw>
2020-05-26 18:09:16 +0800

    add _test.py and run.py

--- python/src/run.py
+++ python/src/run.py
@@ -6,14 +6,15 @@ import json
 from pprint import pprint
 from datetime import datetime
 import requests
+import traceback
 
 from _api_client import DSApiClient
 import _func
 
 
 def main(dsapi=DSApiClient()):
-    _func.policies_generation(dsapi, 'computer_properties.json')
-    #_func.policies_dump(dsapi, 'computer_properties.json')
+    #_func.policies_operation(dsapi, 'computer_properties.json')
+    _func.policies_dump(dsapi, 'computer_properties.json')
 
 if __name__ == '__main__':
     main()
